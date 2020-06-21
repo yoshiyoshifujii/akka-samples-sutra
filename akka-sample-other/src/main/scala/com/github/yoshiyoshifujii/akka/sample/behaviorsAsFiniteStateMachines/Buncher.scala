bf449f1abf9567891a81e01d95b48f03856d9daa
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2020-06-21 20:21:06 +0900

    add Behaviors as finite state machines

---
+++ akka-sample-other/src/main/scala/com/github/yoshiyoshifujii/akka/sample/behaviorsAsFiniteStateMachines/Buncher.scala
@@ -0,0 +1,54 @@
+package com.github.yoshiyoshifujii.akka.sample.behaviorsAsFiniteStateMachines
+
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior}
+
+import scala.concurrent.duration._
+
+object Buncher {
+
+  type SomeObj = Any
+
+  final case class Batch(obj: Seq[SomeObj])
+
+  sealed trait Event
+  final case class SetTarget(ref: ActorRef[Batch]) extends Event
+  final case class Queue(obj: SomeObj) extends Event
+  case object Flush extends Event
+  private case object Timeout extends Event
+
+  sealed trait Data
+  case object Uninitialized extends Data
+  final case class Todo(target: ActorRef[Batch], queue: Seq[SomeObj]) extends Data
+
+  private def active(data: Todo): Behavior[Event] =
+    Behaviors.withTimers[Event] { timers =>
+      timers.startSingleTimer(Timeout, 1.second)
+      Behaviors.receiveMessagePartial {
+        case Flush | Timeout =>
+          data.target ! Batch(data.queue)
+          idle(data.copy(queue = Vector.empty))
+        case Queue(obj) =>
+          active(data.copy(queue = data.queue :+ obj))
+      }
+    }
+
+  private def idle(data: Data): Behavior[Event] =
+    Behaviors.setup { context =>
+      Behaviors.receiveMessage[Event] { message =>
+        (message, data) match {
+          case (SetTarget(ref), Uninitialized) =>
+            idle(Todo(ref, Vector.empty))
+          case (Queue(obj), t @ Todo(_, v)) =>
+            active(t.copy(queue = v :+ obj))
+          case other =>
+            context.log.info("idle behaviors unhandled. [{}]", other)
+            Behaviors.unhandled
+        }
+      }
+    }
+
+  def apply(): Behavior[Event] = idle(Uninitialized)
+
+}
+
---
+++ akka-sample-other/src/test/scala/com/github/yoshiyoshifujii/akka/sample/behaviorsAsFiniteStateMachines/BuncherSpec.scala
@@ -0,0 +1,46 @@
+package com.github.yoshiyoshifujii.akka.sample.behaviorsAsFiniteStateMachines
+
+import akka.actor.testkit.typed.scaladsl.ActorTestKit
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.concurrent.{Eventually, ScalaFutures}
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.time.{Seconds, Span}
+
+import scala.concurrent.ExecutionContext
+
+class BuncherSpec extends AnyFreeSpec with BeforeAndAfterAll with ScalaFutures with Eventually {
+
+  private val testKit = ActorTestKit()
+
+  override protected def afterAll(): Unit = {
+    testKit.shutdownTestKit()
+    super.afterAll()
+  }
+
+  override implicit def patienceConfig: PatienceConfig =
+    PatienceConfig(timeout = scaled(Span(60, Seconds)), interval = scaled(Span(1, Seconds)))
+
+
+  "Buncher" - {
+
+    "success" in {
+      val buncherRef = testKit.spawn(Buncher())
+
+      implicit val ex: ExecutionContext = testKit.system.executionContext
+
+      val probe = testKit.createTestProbe[Buncher.Batch]
+
+      buncherRef ! Buncher.Queue("ouch!!")
+
+      buncherRef ! Buncher.SetTarget(probe.ref)
+      buncherRef ! Buncher.Queue("obj-1")
+      buncherRef ! Buncher.Flush
+      probe.expectMessage(Buncher.Batch(Seq("obj-1")))
+
+      buncherRef ! Buncher.Queue("obj-2")
+      buncherRef ! Buncher.Queue("obj-3")
+      probe.expectMessage(Buncher.Batch(Seq("obj-2", "obj-3")))
+    }
+
+  }
+}
