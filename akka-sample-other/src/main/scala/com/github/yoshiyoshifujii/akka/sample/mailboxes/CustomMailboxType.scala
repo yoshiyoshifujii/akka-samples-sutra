dce146886c4f4a285372e926abde48e0a30de958
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2020-06-25 21:01:04 +0900

    add Mailboxes

---
+++ akka-sample-other/src/main/scala/com/github/yoshiyoshifujii/akka/sample/mailboxes/CustomMailboxType.scala
@@ -0,0 +1,43 @@
+package com.github.yoshiyoshifujii.akka.sample.mailboxes
+
+import java.util.concurrent.ConcurrentLinkedQueue
+
+import akka.actor.{ActorRef, ActorSystem}
+import akka.dispatch.{Envelope, MailboxType, MessageQueue, ProducesMessageQueue}
+import com.typesafe.config.Config
+
+object CustomMailboxType extends App {
+
+  trait MyUnboundedMessageQueueSemantics
+
+  object MyUnboundedMailbox {
+    class MyMessageQueue extends MessageQueue with MyUnboundedMessageQueueSemantics {
+
+      private final val queue = new ConcurrentLinkedQueue[Envelope]()
+
+      override def enqueue(receiver: ActorRef, handle: Envelope): Unit =
+        queue.offer(handle)
+
+      override def dequeue(): Envelope = queue.poll()
+
+      override def numberOfMessages: Int = queue.size
+
+      override def hasMessages: Boolean = !queue.isEmpty
+
+      override def cleanUp(owner: ActorRef, deadLetters: MessageQueue): Unit =
+        while (hasMessages) {
+          deadLetters.enqueue(owner, dequeue())
+        }
+    }
+  }
+
+  class MyUnboundedMailbox extends MailboxType with ProducesMessageQueue[MyUnboundedMailbox.MyMessageQueue] {
+
+    import MyUnboundedMailbox._
+
+    def this(settings: ActorSystem.Settings, config: Config) = this()
+
+    override def create(owner: Option[ActorRef], system: Option[ActorSystem]): MessageQueue = new MyMessageQueue()
+  }
+
+}
---
+++ akka-sample-other/src/main/scala/com/github/yoshiyoshifujii/akka/sample/mailboxes/Main.scala
@@ -0,0 +1,37 @@
+package com.github.yoshiyoshifujii.akka.sample.mailboxes
+
+import akka.actor.typed.{ActorSystem, Behavior, MailboxSelector}
+import akka.actor.typed.scaladsl.Behaviors
+import com.typesafe.config.ConfigFactory
+
+object Main extends App {
+
+  object Guardian {
+
+    def childBehavior: Behavior[AnyRef] =
+      Behaviors.setup[AnyRef] { context =>
+        Behaviors.empty
+      }
+
+    def apply(): Behavior[AnyRef] =
+      Behaviors.setup[AnyRef] { context =>
+        context.spawn(childBehavior, "bounded-mailbox-child", MailboxSelector.bounded(100))
+
+        val props = MailboxSelector.fromConfig("my-app.my-special-mailbox")
+        context.spawn(childBehavior, "from-config-mailbox-child", props)
+        Behaviors.empty
+      }
+
+  }
+
+  val system = ActorSystem(Guardian(), "mailboxes", ConfigFactory.parseString(
+    """
+      |my-app {
+      |  my-special-mailbox {
+      |    mailbox-type = "akka.dispatch.SingleConsumerOnlyUnboundedMailbox"
+      |  }
+      |}
+      |""".stripMargin))
+
+  system.terminate()
+}
