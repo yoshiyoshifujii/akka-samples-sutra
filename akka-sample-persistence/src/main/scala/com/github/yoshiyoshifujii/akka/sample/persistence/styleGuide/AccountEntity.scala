df8ff113835b77f8675dc7118fccbed739057358
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2020-07-04 16:15:07 +0900

    add account entity

---
+++ akka-sample-persistence/src/main/scala/com/github/yoshiyoshifujii/akka/sample/persistence/styleGuide/AccountEntity.scala
@@ -0,0 +1,126 @@
+package com.github.yoshiyoshifujii.akka.sample.persistence.styleGuide
+
+import akka.actor.typed.{ ActorRef, Behavior }
+import akka.persistence.typed.PersistenceId
+import akka.persistence.typed.scaladsl.{ Effect, EventSourcedBehavior, ReplyEffect }
+import com.github.yoshiyoshifujii.akka.sample.persistence.serialization.CborSerializable
+
+object AccountEntity {
+
+  // Reply
+  sealed trait CommandReply                            extends CborSerializable
+  sealed trait OperationResult                         extends CommandReply
+  case object Confirmed                                extends OperationResult
+  final case class Rejected(reason: String)            extends OperationResult
+  final case class CurrentBalance(balance: BigDecimal) extends CommandReply
+
+  // Command
+  sealed trait Command                                                              extends CborSerializable
+  final case class CreateAccount(replyTo: ActorRef[OperationResult])                extends Command
+  final case class Deposit(amount: BigDecimal, replyTo: ActorRef[OperationResult])  extends Command
+  final case class Withdraw(amount: BigDecimal, replyTo: ActorRef[OperationResult]) extends Command
+  final case class GetBalance(replyTo: ActorRef[CurrentBalance])                    extends Command
+  final case class CloseAccount(replyTo: ActorRef[OperationResult])                 extends Command
+
+  // Event
+  sealed trait Event                       extends CborSerializable
+  case object AccountCreated               extends Event
+  case class Deposited(amount: BigDecimal) extends Event
+  case class Withdrawn(amount: BigDecimal) extends Event
+  case object AccountClosed                extends Event
+
+  val Zero = BigDecimal(0)
+
+  // State
+  sealed trait Account extends CborSerializable {
+    def applyEvent(event: Event): Account
+  }
+
+  case object EmptyAccount extends Account {
+
+    override def applyEvent(event: Event): Account =
+      event match {
+        case AccountCreated => OpenedAccount(Zero)
+        case _              => throw new IllegalStateException(s"unexpected event [$event] in state [EmptyAccount]")
+      }
+  }
+
+  case class OpenedAccount(balance: BigDecimal) extends Account {
+    require(balance >= Zero, "Account balance can't be negative")
+
+    override def applyEvent(event: Event): Account =
+      event match {
+        case Deposited(amount) => this.copy(balance = balance + amount)
+        case Withdrawn(amount) => this.copy(balance = balance - amount)
+        case AccountClosed     => ClosedAccount
+        case AccountCreated    => throw new IllegalStateException(s"unexpected event [$event] in state [OpenedAccount]")
+      }
+
+    def canWithdraw(amount: BigDecimal): Boolean = balance - amount > Zero
+  }
+
+  case object ClosedAccount extends Account {
+
+    override def applyEvent(event: Event): Account =
+      throw new IllegalStateException(s"unexpected event [$event] in state [ClosedAccount]")
+  }
+
+//  val TypeKey: EntityTypeKey[Command] = EntityTypeKey[Command]("Account")
+
+  private def createAccount(cmd: CreateAccount): ReplyEffect[Event, Account] =
+    Effect.persist(AccountCreated).thenReply(cmd.replyTo)(_ => Confirmed)
+
+  private def deposit(cmd: Deposit): ReplyEffect[Event, Account] =
+    Effect.persist(Deposited(cmd.amount)).thenReply(cmd.replyTo)(_ => Confirmed)
+
+  private def withdraw(acc: OpenedAccount, cmd: Withdraw): ReplyEffect[Event, Account] =
+    if (acc.canWithdraw(cmd.amount))
+      Effect.persist(Withdrawn(cmd.amount)).thenReply(cmd.replyTo)(_ => Confirmed)
+    else
+      Effect.reply(cmd.replyTo)(Rejected(s"Insufficient balance ${acc.balance} to be able to withdraw ${cmd.amount}"))
+
+  private def getBalance(acc: OpenedAccount, cmd: GetBalance): ReplyEffect[Event, Account] =
+    Effect.reply(cmd.replyTo)(CurrentBalance(acc.balance))
+
+  private def closeAccount(acc: OpenedAccount, cmd: CloseAccount): ReplyEffect[Event, Account] =
+    if (acc.balance == Zero)
+      Effect.persist(AccountClosed).thenReply(cmd.replyTo)(_ => Confirmed)
+    else
+      Effect.reply(cmd.replyTo)(Rejected("Can't close account with non-zero balance"))
+
+  private def replyClosed(accountNumber: String, replyTo: ActorRef[OperationResult]): ReplyEffect[Event, Account] =
+    Effect.reply(replyTo)(Rejected(s"Account $accountNumber is closed"))
+
+  private def commandHandler(accountNumber: String): (Account, Command) => ReplyEffect[Event, Account] =
+    (state, cmd) =>
+      state match {
+        case EmptyAccount =>
+          cmd match {
+            case c: CreateAccount => createAccount(c)
+            case _                => Effect.unhandled.thenNoReply()
+          }
+
+        case acc @ OpenedAccount(_) =>
+          cmd match {
+            case c: Deposit       => deposit(c)
+            case c: Withdraw      => withdraw(acc, c)
+            case c: GetBalance    => getBalance(acc, c)
+            case c: CloseAccount  => closeAccount(acc, c)
+            case c: CreateAccount => Effect.reply(c.replyTo)(Rejected(s"Account $accountNumber is already created"))
+          }
+
+        case ClosedAccount =>
+          cmd match {
+            case c: Deposit             => replyClosed(accountNumber, c.replyTo)
+            case c: Withdraw            => replyClosed(accountNumber, c.replyTo)
+            case GetBalance(replyTo)    => Effect.reply(replyTo)(CurrentBalance(Zero))
+            case CloseAccount(replyTo)  => replyClosed(accountNumber, replyTo)
+            case CreateAccount(replyTo) => replyClosed(accountNumber, replyTo)
+          }
+      }
+
+  private val eventHandler: (Account, Event) => Account = (state, event) => state.applyEvent(event)
+
+  def apply(accountNumber: String, persistenceId: PersistenceId): Behavior[Command] =
+    EventSourcedBehavior.withEnforcedReplies(persistenceId, EmptyAccount, commandHandler(accountNumber), eventHandler)
+}
---
+++ akka-sample-persistence/src/main/scala/com/github/yoshiyoshifujii/akka/sample/persistence/styleGuide/AccountEntity3.scala
@@ -0,0 +1,129 @@
+package com.github.yoshiyoshifujii.akka.sample.persistence.styleGuide
+
+import akka.actor.typed.{ ActorRef, Behavior }
+import akka.persistence.typed.PersistenceId
+import akka.persistence.typed.scaladsl.{ Effect, EventSourcedBehavior }
+import com.github.yoshiyoshifujii.akka.sample.persistence.serialization.CborSerializable
+
+object AccountEntity3 {
+
+  // Reply
+  sealed trait CommandReply                            extends CborSerializable
+  sealed trait OperationResult                         extends CommandReply
+  case object Confirmed                                extends OperationResult
+  final case class Rejected(reason: String)            extends OperationResult
+  final case class CurrentBalance(balance: BigDecimal) extends CommandReply
+
+  // Command
+  sealed trait Command                                                              extends CborSerializable
+  final case class CreateAccount(replyTo: ActorRef[OperationResult])                extends Command
+  final case class Deposit(amount: BigDecimal, replyTo: ActorRef[OperationResult])  extends Command
+  final case class Withdraw(amount: BigDecimal, replyTo: ActorRef[OperationResult]) extends Command
+  final case class GetBalance(replyTo: ActorRef[CurrentBalance])                    extends Command
+  final case class CloseAccount(replyTo: ActorRef[OperationResult])                 extends Command
+
+  // Event
+  sealed trait Event                       extends CborSerializable
+  case object AccountCreated               extends Event
+  case class Deposited(amount: BigDecimal) extends Event
+  case class Withdrawn(amount: BigDecimal) extends Event
+  case object AccountClosed                extends Event
+
+  val Zero = BigDecimal(0)
+
+  type ReplyEffect = akka.persistence.typed.scaladsl.ReplyEffect[Event, Option[Account]]
+
+  // State
+  sealed trait Account extends CborSerializable {
+    def applyCommand(cmd: Command): ReplyEffect
+    def applyEvent(event: Event): Account
+  }
+
+  case class OpenedAccount(balance: BigDecimal) extends Account {
+    require(balance >= Zero, "Account balance can't be negative")
+
+    override def applyCommand(cmd: Command): ReplyEffect =
+      cmd match {
+        case Deposit(amount, replyTo) =>
+          Effect.persist(Deposited(amount)).thenReply(replyTo)(_ => Confirmed)
+
+        case Withdraw(amount, replyTo) =>
+          if (canWithdraw(amount))
+            Effect.persist(Withdrawn(amount)).thenReply(replyTo)(_ => Confirmed)
+          else
+            Effect.reply(replyTo)(Rejected(s"Insufficient balance $balance to be able to withdraw $amount"))
+
+        case GetBalance(replyTo) =>
+          Effect.reply(replyTo)(CurrentBalance(balance))
+
+        case CloseAccount(replyTo) =>
+          if (balance == Zero)
+            Effect.persist(AccountClosed).thenReply(replyTo)(_ => Confirmed)
+          else
+            Effect.reply(replyTo)(Rejected("Can't close account with non-zero balance"))
+
+        case c: CreateAccount =>
+          Effect.reply(c.replyTo)(Rejected("Account is already created"))
+      }
+
+    override def applyEvent(event: Event): Account =
+      event match {
+        case Deposited(amount) => this.copy(balance = balance + amount)
+        case Withdrawn(amount) => this.copy(balance = balance - amount)
+        case AccountClosed     => ClosedAccount
+        case AccountCreated    => throw new IllegalStateException(s"unexpected event [$event] in state [OpenedAccount]")
+      }
+
+    def canWithdraw(amount: BigDecimal): Boolean = balance - amount > Zero
+  }
+
+  case object ClosedAccount extends Account {
+
+    private def replyClosed(replyTo: ActorRef[OperationResult]): ReplyEffect =
+      Effect.reply(replyTo)(Rejected(s"Account is closed"))
+
+    override def applyCommand(cmd: Command): ReplyEffect =
+      cmd match {
+        case c: Deposit             => replyClosed(c.replyTo)
+        case c: Withdraw            => replyClosed(c.replyTo)
+        case GetBalance(replyTo)    => Effect.reply(replyTo)(CurrentBalance(Zero))
+        case CloseAccount(replyTo)  => replyClosed(replyTo)
+        case CreateAccount(replyTo) => replyClosed(replyTo)
+      }
+
+    override def applyEvent(event: Event): Account =
+      throw new IllegalStateException(s"unexpected event [$event] in state [ClosedAccount]")
+  }
+
+  //  val TypeKey: EntityTypeKey[Command] = EntityTypeKey[Command]("Account")
+
+  private def onFirstCommand(cmd: Command): ReplyEffect =
+    cmd match {
+      case CreateAccount(replyTo) =>
+        Effect.persist(AccountCreated).thenReply(replyTo)(_ => Confirmed)
+      case _ =>
+        Effect.unhandled.thenNoReply()
+    }
+
+  private def onFirstEvent(event: Event): Account =
+    event match {
+      case AccountCreated => OpenedAccount(Zero)
+      case _              => throw new IllegalStateException(s"unexpected event [$event] in state [EmptyAccount]")
+    }
+
+  def apply(persistenceId: PersistenceId): Behavior[Command] =
+    EventSourcedBehavior.withEnforcedReplies[Command, Event, Option[Account]](
+      persistenceId,
+      None,
+      (state, cmd) =>
+        state match {
+          case None          => onFirstCommand(cmd)
+          case Some(account) => account.applyCommand(cmd)
+        },
+      (state, event) =>
+        state match {
+          case None          => Some(onFirstEvent(event))
+          case Some(account) => Some(account.applyEvent(event))
+        }
+    )
+}
